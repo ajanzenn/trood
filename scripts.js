cd931cbfb37f9ae71a14bb389b7e524b2006dc2b
ajanzenn <janzen.sasha@gmail.com>
2021-03-26 21:02:55 -0700

    added: mood buttons working journal saving jspaint iframe

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let d = new Date();
+    function saveFile() {
+        let journalInput = $('#journalInput').val();
+        if (journalInput) {
+            let textBlob = new Blob([journalInput], {type:"text/plain"});
+            let URL = window.URL.createObjectURL(textBlob);
+            let fileName = d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
+
+            let downloadLink = document.createElement('a');
+            downloadLink.download = fileName;
+            downloadLink.innerHTML = "Download Journal Entry";
+            downloadLink.href = URL;
+            downloadLink.onclick = destroyClickedElement;
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+
+            downloadLink.click();
+            location.reload();
+           
+        } else{ $('#error').html('Please write something before saving your journal.'); }
+    }
+
+    function destroyClickedElement(e){
+        document.body.remove(e.target);
+    } 
